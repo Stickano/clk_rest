340a4d84a032519ffac0c1e9a79ebbcf57bcb861
Henrik Jeppesen <Jeppesen@tutanota.com>
2018-12-13 05:27:41 +0100

    Initial

---
+++ clk_rest/Resources/Server.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace clk_rest.Resources
+{
+    public class Server
+    {
+        public static string db = "";
+
+    }
+}
