0a679b3c30d812e2a650c85a16c3ed10c78035cc
Henrik Jeppesen <Jeppesen@tutanota.com>
2018-12-16 07:18:54 +0100

    Added methods to receive all boards and individual boards

--- clk_rest/IService1.cs
+++ clk_rest/IService1.cs
@@ -12,6 +12,11 @@ namespace clk_rest
     [ServiceContract]
     public interface IService1
     {
+        /// <summary>
+        /// Create a profile.
+        /// </summary>
+        /// <param name="profile">The email and password (in hashed state) is required.</param>
+        /// <returns>The newly created profile (contains UID)</returns>
         [WebInvoke(UriTemplate = "profile/create",
             Method = "POST",
             RequestFormat = WebMessageFormat.Json,
@@ -20,6 +25,11 @@ namespace clk_rest
         [OperationContract]
         int createProfile(Profile profile);
 
+        /// <summary>
+        /// Match login values (email & hashed password) to a profile in the database.
+        /// </summary>
+        /// <param name="profile">Email and Password (hashed) to match against in the database.</param>
+        /// <returns>The profile of the user (UID included)</returns>
         [WebInvoke(UriTemplate = "profile/login",
             Method = "POST",
             RequestFormat = WebMessageFormat.Json,
@@ -28,6 +38,11 @@ namespace clk_rest
         [OperationContract]
         Profile login(Profile profile);
 
+        /// <summary>
+        /// Save a board to the database.
+        /// </summary>
+        /// <param name="board">(BoardController) Board, Lists, Cards, Checklists, Points, Comments, User ID and Password (hashed)</param>
+        /// <returns>-1 on fail, 1 on success</returns> TODO: Should prolly give more informative feedback
         [WebInvoke(UriTemplate = "board/save",
             Method = "POST",
             RequestFormat = WebMessageFormat.Json,
@@ -36,10 +51,37 @@ namespace clk_rest
         [OperationContract]
         int saveBoard(Board board);
 
-        //[WebInvoke(UriTemplate = "peek",
+        /// <summary>
+        /// Get all boards associated with a user.
+        /// </summary>
+        /// <param name="user">The user to find associated boards for (UID and Password (hashed) required)</param>
+        /// <returns>All boards associated with the profile</returns>
+        [WebInvoke(UriTemplate = "board/getall",
+            Method = "POST",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Bare)]
+        [OperationContract]
+        IList<Board> getBoards(Profile user);
+
+        /// <summary>
+        /// Get specific board from the database.
+        /// </summary>
+        /// <param name="user">The user that is fetching the board (UID and hashed password required)</param>
+        /// <param name="boardId">The ID of the board to fetch</param>
+        /// <returns>The board, full with lists, cards, checklists, points and comments</returns>
+        [WebInvoke(UriTemplate = "board/get/{boardId}",
+            Method = "POST",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Bare)]
+        [OperationContract]
+        Board getBoard(Profile user, string boardId);
+
+
+        //[WebInvoke(UriTemplate = "board/getall",
         //    Method = "GET",
         //    ResponseFormat = WebMessageFormat.Json)]
         //[OperationContract]
-
     }
 }
--- clk_rest/Service1.svc.cs
+++ clk_rest/Service1.svc.cs
@@ -197,17 +197,23 @@ namespace clk_rest
             }
 
             // Create checklist points (..doh  description = name)
-            sql = "INSERT INTO checklist_points (ukey, description, created, checklist_id) VALUES (@ukey, @name, @created, @checklist_id)";
+            sql = "INSERT INTO checklist_points (ukey, description, created, checklist_id, checked) VALUES (@ukey, @name, @created, @checklist_id, @checked)";
             using (SqlConnection conn = new SqlConnection(db))
             using (SqlCommand query = new SqlCommand(sql, conn))
             {
                 foreach (ChecklistPoint point in board.points)
                 {
+
+                    int isCheck = 0;
+                    if (point.isCheck)
+                        isCheck = 1;
+
                     query.Parameters.Clear();
                     query.Parameters.AddWithValue("@ukey", point.id);
                     query.Parameters.AddWithValue("@name", point.name);
                     query.Parameters.AddWithValue("@created", point.created);
                     query.Parameters.AddWithValue("@checklist_id", point.checklistId);
+                    query.Parameters.AddWithValue("@checked", isCheck);
 
                     conn.Open();
                     query.ExecuteNonQuery();
@@ -236,6 +242,230 @@ namespace clk_rest
             }
 
             return 1;
+        }
+
+        /// <summary>
+        /// Get all boards associated to a profile.
+        /// </summary>
+        /// <param name="user">A valid profile</param>
+        /// <returns>A list of all associated boards to that user</returns>
+        public IList<Board> getBoards(Profile user)
+        {
+            IList<Board> boards = new List<Board>();
+
+            // Confirm profile
+            Profile p = login(user);
+            if (p.id == null)
+                return boards;
+
+            string sql = "SELECT ukey, name FROM boards WHERE user_id=@uid";
+            using (SqlConnection conn = new SqlConnection(db))
+            using (SqlCommand query = new SqlCommand(sql, conn))
+            {
+                query.Parameters.AddWithValue("@uid", p.id);
+                conn.Open();
+                using (SqlDataReader result = query.ExecuteReader())
+                {
+                    // If user was NOT found, return an empty token
+                    if (!result.HasRows)
+                        return boards;
+
+                    while (result.Read())
+                    {
+                        boards.Add(new Board
+                        {
+                            name = result["name"].ToString(),
+                            id = result["ukey"].ToString()
+                        });
+                    }
+
+                    conn.Close();
+                    return boards;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Get a specific board, with all its lists, cards etc.
+        /// </summary>
+        /// <param name="user">A valid user for the board</param>
+        /// <param name="boardId">The UID for the board</param>
+        /// <returns>(BoardController) Board with all its content</returns>
+        public Board getBoard(Profile user, string boardId)
+        {
+            Board b = new Board();
+
+            // Confirm user
+            Profile p = login(user);
+            if (p.id == null)
+                return b;
+
+            // Get board
+            string sql = "SELECT * FROM boards WHERE ukey=@boardId AND active=1";
+            using (SqlConnection conn = new SqlConnection(db))
+            using (SqlCommand query = new SqlCommand(sql, conn))
+            {
+                query.Parameters.AddWithValue("@boardId", boardId);
+                conn.Open();
+                using (SqlDataReader result = query.ExecuteReader())
+                {
+                    // If user was NOT found, return an empty token
+                    if (!result.HasRows)
+                        return b;
+
+                    result.Read();
+                    b.created = result["created"].ToString();
+                    b.name = result["name"].ToString();
+                    b.id = result["ukey"].ToString();
+
+                    conn.Close();
+                }
+            }
+
+            // Get lists
+            sql = "SELECT * FROM lists WHERE board_id=@boardId AND active=1";
+            using (SqlConnection conn = new SqlConnection(db))
+            using (SqlCommand query = new SqlCommand(sql, conn))
+            {
+                query.Parameters.AddWithValue("@boardId", boardId);
+                conn.Open();
+                using (SqlDataReader result = query.ExecuteReader())
+                {
+                    while (result.Read())
+                    {
+                        b.lists.Add(new List
+                        {
+                            boardId = result["board_id"].ToString(),
+                            id = result["ukey"].ToString(),
+                            name = result["name"].ToString(),
+                            created = result["created"].ToString()
+                        });
+                    }
+                    
+                    conn.Close();
+                }
+            }
+
+            // Get cards
+            sql = "SELECT * FROM cards WHERE list_id=@listId AND active=1";
+            using (SqlConnection conn = new SqlConnection(db))
+            using (SqlCommand query = new SqlCommand(sql, conn))
+            {
+                foreach (List list in b.lists)
+                {
+                    query.Parameters.Clear();
+                    query.Parameters.AddWithValue("@listId", list.id);
+                    conn.Open();
+                    using (SqlDataReader result = query.ExecuteReader())
+                    {
+                        while (result.Read())
+                        {
+                            b.cards.Add(new Card
+                            {
+                                listId = result["list_id"].ToString(),
+                                id = result["ukey"].ToString(),
+                                name = result["description"].ToString(),
+                                created = result["created"].ToString()
+                            });
+                        }
+
+                        conn.Close();
+                    }
+                }
+                
+            }
+
+            // Get checklists
+            sql = "SELECT * FROM checklists WHERE card_id=@cardId AND active=1";
+            using (SqlConnection conn = new SqlConnection(db))
+            using (SqlCommand query = new SqlCommand(sql, conn))
+            {
+                foreach (Card card in b.cards)
+                {
+                    query.Parameters.Clear();
+                    query.Parameters.AddWithValue("@cardId", card.id);
+                    conn.Open();
+                    using (SqlDataReader result = query.ExecuteReader())
+                    {
+                        while (result.Read())
+                        {
+                            b.checklists.Add(new Checklist
+                            {
+                                cardId = result["card_id"].ToString(),
+                                id = result["ukey"].ToString(),
+                                name = result["name"].ToString(),
+                                created = result["created"].ToString()
+                            });
+                        }
+
+                        conn.Close();
+                    }
+                }
+
+            }
+
+            // Get checklist points
+            sql = "SELECT * FROM checklist_points WHERE checklist_id=@checkId AND active=1";
+            using (SqlConnection conn = new SqlConnection(db))
+            using (SqlCommand query = new SqlCommand(sql, conn))
+            {
+                foreach (Checklist check in b.checklists)
+                {
+                    query.Parameters.Clear();
+                    query.Parameters.AddWithValue("@checkId", check.id);
+                    conn.Open();
+                    using (SqlDataReader result = query.ExecuteReader())
+                    {
+                        while (result.Read())
+                        {
+                            bool isCheck = result["checked"].ToString().Equals("1");
+
+                            b.points.Add(new ChecklistPoint
+                            {
+                                checklistId = result["checklist_id"].ToString(),
+                                id = result["ukey"].ToString(),
+                                name = result["description"].ToString(),
+                                created = result["created"].ToString(),
+                                isCheck = isCheck
+                            });
+                        }
+
+                        conn.Close();
+                    }
+                }
+
+            }
+
+            // Get comments
+            sql = "SELECT * FROM comments WHERE card_id=@cardId AND active=1";
+            using (SqlConnection conn = new SqlConnection(db))
+            using (SqlCommand query = new SqlCommand(sql, conn))
+            {
+                foreach (Card card in b.cards)
+                {
+                    query.Parameters.Clear();
+                    query.Parameters.AddWithValue("@cardId", card.id);
+                    conn.Open();
+                    using (SqlDataReader result = query.ExecuteReader())
+                    {
+                        while (result.Read())
+                        {
+                            b.comments.Add(new Comment
+                            {
+                                cardId = result["card_id"].ToString(),
+                                id = result["ukey"].ToString(),
+                                comment = result["comment"].ToString(),
+                                created = result["created"].ToString(),
+                                userId = result["user_id"].ToString()
+                            });
+                        }
+
+                        conn.Close();
+                    }
+                }
+
+                return b;
+            }
         }
     }
 }
