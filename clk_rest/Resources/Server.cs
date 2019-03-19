dc49ad010557410b0ff8115c2a05c43026dc7773
Henrik Jeppesen <Jeppesen@tutanota.com>
2019-03-19 19:51:54 +0100

    Update Server.cs

--- clk_rest/Resources/Server.cs
+++ clk_rest/Resources/Server.cs
@@ -7,6 +7,6 @@ namespace clk_rest.Resources
 {
     public class Server
     {
-        public static string db = "Server=tcp:microshaft.database.windows.net,1433;Initial Catalog=clk;Persist Security Info=False;User ID=stickano;Password=fx%4vnr2;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        public static string db = "Server=tcp:microshaft.database.windows.net,1433;Initial Catalog=clk;Persist Security Info=False;User ID=;Password=;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
     }
 }
